62b6f3833b0609c8e9de9a183a84685b87c1ec75
dxtAshish <ashishdxt121200@gmail.com>
2024-06-04 12:55:45 +0530

    add uploadThing

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -21,6 +21,7 @@ import {
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { useEffect, useState } from "react";
+import { FileUpload } from "../file-upload";
 
 
 const formSchema = z.object({
@@ -68,7 +69,24 @@ export const InitialModal = ()=>{
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
              <div className="space-y-8 px-6"> 
               <div className="flex items-center justify-center text-center">
-             Todo:Image Upload
+            <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({field})=>(
+            <FormItem>
+              <FormControl>
+               <FileUpload
+               endpoint="serverImage"
+               value={field.value}
+               onChange={field.onChange}
+               />
+              </FormControl>
+            
+            </FormItem>
+            )}
+            />
+
+           
               </div>
               <FormField
               control={form.control}
